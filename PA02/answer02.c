0f6a246fed8c534438ddbbcd6be7bb0e84f53bc9
Jordan Huffaker <jordhuff@gmail.com>
2014-09-07 18:58:24 -0400

    1st function coded.

--- PA02/answer02.c
+++ PA02/answer02.c
@@ -2,5 +2,52 @@
 
 size_t my_strlen(const char * str)
 {
+	int i = 0;
+
+	while (str[i] != '\0')
+	{
+		i++;
+	}
+
+	return i;
+}
+
+int my_countchar(const char * str, char ch)
+{
+	return 0;
+}
+
+char * my_strchr(const char * str, int ch)
+{
+	return 'd';
+}
+
+char * my_strrchr(const char * str, int ch)
+{
+	return 'd';
+}
+
+char * my_strstr(const char * haystack, const char * needle)
+{
+	return 's';
+}
+
+char * my_strcpy(char * dest, const char * src)
+{
+	return 's';
+}
+
+char * my_strcat(char * dest, const char * src)
+{
+	return 's';
+}
+
+int my_isspace(int ch)
+{
+	return 0;
+}
+
+int my_atoi(const char * str)
+{
 	return 0;
 }
