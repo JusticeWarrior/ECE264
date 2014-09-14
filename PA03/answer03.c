7d2ba52290159b83d430e32d8c92b6b34f32705d
Jordan Huffaker <jordhuff@gmail.com>
2014-09-14 19:17:30 -0400

    Add a new Visual Studios project and C++ Unit Test project to the repo.

---
+++ PA03/answer03.c
@@ -0,0 +1,38 @@
+#include <stdlib.h>
+
+char * strcat_ex(char * * dest, int * n, const char * src)
+{
+	char d = 's';
+
+	return &d;
+}
+
+char * * explode(const char * str, const char * delims, int * arrLen)
+{
+	char d = 's';
+	char * s = &d;
+
+	return &s;
+}
+
+char * implode(char * * strArr, int len, const char * glue)
+{
+	char d = 's';
+
+	return &d;
+}
+
+void sortStringArray(char * * arrString, int len)
+{
+
+}
+
+void sortStringCharacters(char * str)
+{
+
+}
+
+void destroyStringArray(char * * strArr, int len)
+{
+
+}
