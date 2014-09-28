ca64d5e02c6cc2a620e96d647aab427722518262
Jordan Huffaker <jordhuff@gmail.com>
2014-09-28 16:55:51 -0400

    Finished cat-lite without unit testing

--- PA04/cat-lite.c
+++ PA04/cat-lite.c
@@ -4,6 +4,15 @@
 
 int main(int argc, char * * argv)
 {
+	if (argc == 1)
+	{
+		while (!feof(stdin))
+		{
+			fputc(fgetc(stdin), stdout);
+		}
+		return EXIT_SUCCESS;
+	}
+
 	int i;
 	for (i = 1; i < argc; i++)
 	{
@@ -20,6 +29,31 @@ int main(int argc, char * * argv)
 			fprintf(stdout, "</help-message>\n");
 			return EXIT_SUCCESS;
 		}
+	}
+
+	for (i = 1; i < argc; i++)
+	{
+		if (!strcmp(argv[i], "-"))
+		{
+			while (!feof(stdin))
+			{
+				fputc(fgetc(stdin), stdout);
+			}
+			continue;
+		}
+
+		FILE * fp = fopen(argv[i], "r");
+		if (fp == NULL)
+		{
+			fprintf(stderr, "cat cannot open %s\n", argv[i]);
+			return EXIT_FAILURE;
+		}
+		
+		while (!feof(fp))
+		{
+			fputc(fgetc(fp), stdout);
+		}
+		fclose(fp);
 	}
 
 	return EXIT_SUCCESS;
