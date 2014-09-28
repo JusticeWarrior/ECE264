fa2acc6779803a5467a1a16a8a3465cfecc737e2
Jordan Huffaker <jordhuff@gmail.com>
2014-09-28 17:12:24 -0400

    Added help switch to grep-lite function

--- PA04/grep-lite.c
+++ PA04/grep-lite.c
@@ -1,12 +1,28 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 int main(int argc, char * * argv)
 {
-	int ind;
-
-	for (ind = 1; ind < argc; ++ind) {
-		printf("%s ", ind, argv[ind]);
+	int i;
+	for (i = 1; i < argc; i++)
+	{
+		if (!strcmp(argv[i], "--help"))
+		{
+			fprintf(stdout, "<help-message>\n");
+			fprintf(stdout, "Usage: grep-lite [OPTION]... PATTERN\n");
+			fprintf(stdout, "Search for PATTERN in standard input. PATTERN is a\n");
+			fprintf(stdout, "string. grep-lite will search standard input line by\n");
+			fprintf(stdout, "line, and (by default) print out those lines which\n");
+			fprintf(stdout, "contain pattern as a substring.\n\n");
+			fprintf(stdout, "  -v, --invert-match     print non-matching lines\n");
+			fprintf(stdout, "  -n, --line-number      print line numbers with output\n");
+			fprintf(stdout, "  -q, --quiet            suppress all output\n\n");
+			fprintf(stdout, "Exit status is 0 if any line is selected, 1 otherwise;\n");
+			fprintf(stdout, "if any error occurs, then the exit status is 2.\n");
+			fprintf(stdout, "</help-message>\n");
+			return EXIT_SUCCESS;
+		}
 	}
 
 	return EXIT_SUCCESS;
