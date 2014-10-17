a2bd25a0d915e130522f2d8521ee7ee0c2cdb947
Jordan Huffaker <jordhuff@gmail.com>
2014-10-17 16:34:08 -0400

    Created a struct for maze with constructor and deconstructor.

--- PA06/answer06.c
+++ PA06/answer06.c
@@ -1,7 +1,34 @@
 #include "answer06.h"
+#include <stdlib.h>
 #include <stdio.h>
 
-void print_directions(char** maze, int w, int h)
+typedef struct
 {
-	fprintf(stdout, "Balls");
+	char ** maze;
+	int w;
+	int h;
+} Maze;
+
+Maze* Create_Maze(char ** input, int w, int h)
+{
+	Maze* maze = malloc(sizeof(Maze));
+	maze ->maze = input;
+	maze ->w = w;
+	maze ->h = h;
+
+	return maze;
 }
+
+void Dispose_Maze(Maze* maze)
+{
+	free(maze);
+}
+
+void print_directions(char** input, int w, int h)
+{
+	Maze* maze = Create_Maze(input, w, h);
+
+	Dispose_Maze(maze);
+}
+
+//void Print_Directions_Recur(char** maze, int w, int h, )
