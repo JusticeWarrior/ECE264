49c236008a14efd876038411902cbf3bb0cfc3cf
Jordan Huffaker <jordhuff@gmail.com>
2014-11-09 13:32:55 -0500

    Added function definitions to answer09.c

---
+++ PA09/answer09.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+#include "answer09.h"
+
+BusinessNode * create_node(char * stars, char * name, char * address)
+{
+	return NULL;
+}
+
+BusinessNode * tree_insert(BusinessNode * node, BusinessNode * root)
+{
+	return NULL;
+}
+
+BusinessNode * load_tree_from_file(char * filename)
+{
+	return NULL;
+}
+
+BusinessNode * tree_search_name(char * name, BusinessNode * root)
+{
+	return NULL;
+}
+
+void print_node(BusinessNode * node)
+{
+}
+
+void print_tree(BusinessNode * tree)
+{
+
+}
+
+void destroy_tree(BusinessNode * root)
+{
+
+}
