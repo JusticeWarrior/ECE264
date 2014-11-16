c457012e421b6b5016dc2b9b5cd225c9bab54f5f
Jordan Huffaker <jordhuff@gmail.com>
2014-11-16 18:52:55 -0500

    The structs have been created. They will probably change over time.

--- PA10/answer10.c
+++ PA10/answer10.c
@@ -2,6 +2,78 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
+#include "answer10.h"
+
+/* This struct will hold the data read from the file. */
+typedef struct m_TempData
+{
+	int businessID;
+	char * name;
+	char * address;
+	char * city;
+	char * state;
+	char * zipCode;
+} TempData;
+
+/* This struct will hold the review information about a specific store */ 
+typedef struct m_BusinessNode
+{
+	long int reviews;
+	int numReviews;
+} BusinessNode;
+
+/* This tree will be built during a query. */
+typedef struct m_AddressTree
+{
+	char * address;
+	BusinessNode reviews;
+	struct m_AddressTree * left;
+	struct m_AddressTree * right;
+} AddressTree;
+
+/* This tree will be built during a query. */
+typedef struct m_CityTree
+{
+	char * city;
+	AddressTree * addressTree;
+	struct m_CityTree * left;
+	struct m_CityTree * right;
+} CityTree;
+
+/* This struct will hold information about businesses before the other trees are built. */
+typedef struct m_LocalBusiness
+{
+	char * city;
+	char * address;
+	BusinessNode * reviews;
+} LocalBusiness;
+
+/* This tree will be built during the create_business_bst() function. */
+typedef struct m_ZipCodesTree
+{
+	char * zipcode;
+	LocalBusiness * localBusinesses;
+	struct m_ZipCodesTree * left;
+	struct m_ZipCodesTree * right;
+} ZipCodesTree;
+
+/* This tree will be built during the create_business_bst() function. */
+typedef struct m_StatesTree
+{
+	char * state;
+	ZipCodesTree * zipCodeTree;
+	struct m_StatesTree * left;
+	struct m_StatesTree * right;
+} StatesTree;
+
+/* This tree will be built during the create_business_bst() function. */
+typedef struct m_YelpDataBST
+{
+	char * name;
+	StatesTree * stateTree;
+	struct m_YelpDataBST * left;
+	struct m_YelpDataBST * right;
+} YelpDataBST;
 
 struct YelpDataBST* create_business_bst(const char* businesses_path,
 	const char* reviews_path)
--- PA10/pa10.c
+++ PA10/pa10.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
+#include "answer10.h"
 
 int main(int argc, char * * argv)
 {
