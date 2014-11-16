29a358575f122772c5047cfad9a2df015d62297d
Jordan Huffaker <jordhuff@gmail.com>
2014-11-16 16:51:59 -0500

    An outline of the assignment is made, time to do some more planning before I implement anything real.

---
+++ PA10/answer10.c
@@ -0,0 +1,26 @@
+#include <inttypes.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+struct YelpDataBST* create_business_bst(const char* businesses_path,
+	const char* reviews_path)
+{
+	return NULL;
+}
+
+struct Business* get_business_reviews(struct YelpDataBST* bst,
+	char* name, char* state, char* zip_code)
+{
+	return NULL;
+}
+
+void destroy_business_bst(struct YelpDataBST* bst)
+{
+
+}
+
+void destroy_business_result(struct Business* b)
+{
+
+}
