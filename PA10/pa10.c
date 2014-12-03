192431ed764e54e2355aac58275a73ef0c42c34b
Jordan Patrick Huffaker <jhuffak@ecegrid-thin1.ecn.purdue.edu>
2014-12-03 18:32:41 -0500

    Changed review and business file paths to the correct Unix file path under ecegrid

--- PA10/pa10.c
+++ PA10/pa10.c
@@ -6,7 +6,7 @@
 
 int main(int argc, char * * argv)
 {
-	struct YelpDataBST * tree = create_business_bst("businesses.tsv", "reviews.tsv");
+	struct YelpDataBST * tree = create_business_bst("/home/shay/a/ece264p0/share/yelp_data/businesses.tsv", "/home/shay/a/ece264p0/share/yelp_data/reviews.tsv");
 
 	struct Business * business = get_business_reviews(tree, "Boston Cleaners", "NV", NULL);
 
