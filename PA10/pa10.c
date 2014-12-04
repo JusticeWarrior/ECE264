2ef81cb818081e37df3d96d901ce34aca7788dd1
Jordan Patrick Huffaker <jhuffak@ecegrid-thin1.ecn.purdue.edu>
2014-12-03 19:26:28 -0500

    Made some Unix only changes.

--- PA10/pa10.c
+++ PA10/pa10.c
@@ -7,30 +7,11 @@
 int main(int argc, char * * argv)
 {
 	struct YelpDataBST * tree = create_business_bst("/home/shay/a/ece264p0/share/yelp_data/businesses.tsv", "/home/shay/a/ece264p0/share/yelp_data/reviews.tsv");
+	//struct Business * business = get_business_reviews(tree, "Meatballz", "NV", NULL);
 
+	//destroy_business_bst(tree);
 
-	struct Business * business = get_business_reviews(tree, "Meatballz", "NV", NULL);
+	//destroy_business_result(business);
 
-	business->locations->reviews[0];
-	business->locations->reviews[1];
-	business->locations->reviews[2];
-	business->locations->reviews[3];
-	business->locations->reviews[4];
-	business->locations->reviews[5];
-	business->locations->reviews[6];
-	business->locations->reviews[7];
-	business->locations->reviews[8];
-	business->locations->reviews[9];
-	business->locations->reviews[10];
-	business->locations->reviews[11];
-
-	business->locations[1];
-	business->locations[1].reviews[0];
-	business->locations[1].reviews[1];
-	business->locations[1].reviews[2];
-	business->locations[1].reviews[3];
-
-	destroy_business_bst(tree);
-
-	destroy_business_result(business);
+	return EXIT_SUCCESS;
 }
