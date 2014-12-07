8b77e359e5624ac638166fe958a97ef721ad7763
Jordan Huffaker <jordhuff@gmail.com>
2014-12-07 15:20:03 -0500

    Tested first 2 functions with no errors!

--- PA11/pa11.c
+++ PA11/pa11.c
@@ -5,5 +5,11 @@
 
 int main(int argc, char * * argv)
 {
+	HuffNode * node = HuffNode_create(5);
+	node->left = HuffNode_create(6);
+	node->left->right = HuffNode_create(7);
+
+	HuffNode_destroy(node);
+
 	return EXIT_SUCCESS;
 }
