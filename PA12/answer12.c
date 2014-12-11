9ea752d8bd1ac637b71c18aeabcaa04127f27541
Jordan Patrick Huffaker <jhuffak@ee207lnx18.ecn.purdue.edu>
2014-12-11 16:25:21 -0500

    Outline to final function is finished.

--- PA12/answer12.c
+++ PA12/answer12.c
@@ -1,6 +1,8 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
+#include <math.h>
+#include <pthread.h>
 #include "answer12.h"
 
 uint128 alphaTou128(const char * str)
@@ -29,6 +31,9 @@ uint128 alphaTou128(const char * str)
 
 static int u128Length(uint128 value)
 {
+	if (value == 0)
+		return 1;
+
 	int len = 0;
 
 	while(value > 0)
@@ -44,11 +49,11 @@ char * u128ToString(uint128 value)
 {
 	int len = u128Length(value);
 
-	char * str = malloc(sizeof(char) * len + 1);
-	str[len] = '\n';
+	char * str = malloc((sizeof(char) * len) + 1);
+	str[len] = '\0';
 	
 	int i;
-	for (i = len - 1; i >= 0; i--)
+	for (i = len - 1; i >= 0; --i)
 	{
 		str[i] = (value % 10) + '0';
 		value /= 10;
@@ -57,8 +62,57 @@ char * u128ToString(uint128 value)
 	return str;
 }
 
+typedef struct m_PrimalityArgs
+{
+	uint128 * value;
+	uint128 startValue;
+	uint128 endValue;
+	int * isPrime;
+} PrimalityArgs;
+
+static void PrimalityArgsFill(PrimalityArgs * args, uint128 * value, uint128 startValue, uint128 endValue, int * isPrime)
+{
+	args->value = value;
+	args->startValue = startValue;
+	args->endValue = endValue;
+	args->isPrime = isPrime;
+}
+
+static void testPrimalityThread(void * primalityArg)
+{
+	PrimalityArgs * arguments = (PrimalityArgs *)primalityArg;
+
+	int i;
+	for (i = arguments->startValue; i <= arguments->endValue; i++)
+	{
+		if (*(arguments->value) % ((2 * i) + 1) == 0)
+		{
+			*(arguments->isPrime) = FALSE;
+			return;
+		}
+	}
+}
 
 int primalityTestParallel(uint128 value, int n_threads)
 {
-	return 0;
+    	uint128 max = (uint128)floor(sqrtl(value));
+	uint128 threadSize = value / n_threads;
+	int isPrime = TRUE;
+
+	pthread_t * threads[n_threads];	
+	PrimalityArgs args[n_threads];
+
+	PrimalityArgsFill(&args[0], &value, 0, threadSize, &isPrime);
+	pthread_create(threads[0], NULL, testPrimalityThread, &args[0]);
+	
+	int i;
+	for (i = 1; i < n_threads; i++)
+	{
+		
+	}
+
+	if (value == 2)
+		return TRUE;
+
+	return TRUE;
 }
--- PA12/pa12.c
+++ PA12/pa12.c
@@ -5,6 +5,18 @@
 
 int main(int argc, char ** argv)
 {
+	uint128 test1 = alphaTou128("324324325574");
+	char * output1 = u128ToString(test1);
+
+	uint128 test2 = alphaTou128("100000000000000000000000000233232532532");
+	char * output2 = u128ToString(test2);
+
+	fprintf(stdout, "%s", output1);
+	fprintf(stdout, "%s", output2);
+
+	free(output1);
+	free(output2);
+
 	return EXIT_SUCCESS;
 }
 
